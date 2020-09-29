033c7cad60887363bcea5dc1d0f8a4319041d089
bowtiesarecool26 <varunj26012001@gmail.com>
2020-09-29 20:37:15 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,80 @@
+from discord.ext import commands, tasks
+import discord
+import asyncpg
+import os
+import config
+from cogs.help import EmbedHelpCommand
+
+
+TOKEN = config.token
+HOST = config.host
+DATABASE = config.database
+USER = config.user
+PASSWORD = config.password
+
+async def create_db_pool():
+    credentials = {"user": USER, "password": PASSWORD, "database": DATABASE, "host": HOST}
+    try:
+        bot.db = await asyncpg.create_pool(**credentials)
+    except KeyboardInterrupt:
+        await bot.db.close()
+
+async def close_db():
+    await bot.db.close()
+ 
+bot = commands.Bot(command_prefix='$', help_command=EmbedHelpCommand(dm_help=None, dm_help_threshold=10),
+        owner_id=411166117084528640, intents=discord.Intents.all())
+
+@tasks.loop(count=1)
+async def startup():
+    print(f"Logged in as {bot.user}")
+    print(f"ID: {bot.user.id}")
+    print("---------------")
+    bot.owner = bot.get_user(bot.owner_id)
+
+@startup.before_loop
+async def before_start():
+    await bot.wait_until_ready()
+
+@bot.event
+async def on_guild_join(guild):
+    embed=discord.Embed(title="Thank you for adding me to your server :blush:", colour=discord.Colour.blurple())
+    embed.description = f"""I was built to cater to your weekly quiz needs on request of <@!715954704701718608>.
+    I was built hastily, so there might be a few bugs, which you can report on discovery using `$bug` or directly to {bot.owner.mention}.
+
+    Questions and options may be loaded via a .csv file built on the template available [here](https://docs.google.com/spreadsheets/d/1v2KPlZ8fdMqzGRqX9G6dsDAYX_HAxqUeh3TmfSffzRM/edit?usp=sharing). Simply fill out the template, download as .csv and upload it in a channel I can see with the message `$load <static/dynamic> [quiz description]`.
+
+    Reactions once added are removed after recording and cannot be changed.
+
+    Running more than one quiz simultaneously, across all servers, is disallowed. This is because game state cannot be tracked per server without making expensive database calls, since memory is too low to cache. If you wish to run your own instance or contribute to development, I am open source [here](https://github.com/bowtiesarecool26/MedQuizBot/)
+
+    Cheers and happy quizzing!"""
+    embed.set_author(name=guild.me, icon_url=guild.me.avatar_url)
+    embed.set_footer(text=f"Made by {bot.owner}", icon_url=bot.owner.avatar_url)
+    try:
+        await guild.system_channel.send(embed=embed)
+    except:
+        # either the channel is invisible, or no permissions to send messages, or no system channel set
+        # well, we tried
+        pass
+
+bot.load_extension('cogs.med_quiz')
+
+@bot.command(hidden=True)
+async def ping(ctx):
+    """Return bot latency"""
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+
+startup.start()
+try:
+    bot.loop.run_until_complete(create_db_pool())
+    bot.loop.run_until_complete(bot.start(TOKEN))
+except KeyboardInterrupt:
+    bot.loop.run_until_complete(bot.logout())
+    bot.loop.run_until_complete(close_db())
+finally:
+    bot.loop.close()
+    #client.run(TOKEN)
+    startup.cancel()
+
+        
---
+++ cogs/med_quiz.py
@@ -0,0 +1,145 @@
+from discord.ext import commands
+import discord
+import asyncpg
+import asyncio
+import random
+
+MAX_POINTS = 100
+TIMEOUT = 25
+
+class Quiz(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._db = bot.db
+        self.in_play = False
+        self.prepare_db.start()
+        self._current = []
+
+    @tasks.loop(count=1)
+    async def prepare_db(self):
+        await self._db.execute("""CREATE TABLE IF NOT EXISTS quiz_config (id SERIAL PRIMARY KEY, name TEXT, description TEXT);
+        CREATE TABLE IF NOT EXISTS questions (id SERIAL PRIMARY KEY, quiz_id INTEGER, question TEXT, option1 TEXT, option2 TEXT, option3 TEXT, option4 TEXT, correct INTEGER);""")
+        self.ids = []
+        records = self._db.fetch("SELECT * FROM quiz_config")
+        for record in records:
+            self.ids.append(record['id'])
+        print("================\nMed Quiz Cog loaded")
+
+    @prepare_db.before_loop
+    async def before_prepare_db(self):
+        await self.bot.wait_until_ready()
+
+    @commands.command(name="load")
+    @commands.guild_only()
+    async def _load(self, ctx, *, desc=None):
+        """Load a quiz from a CSV file"""
+        if not ctx.message.attachments or not ctx.message.attachments[0].filename.endswith('.csv'):
+            return await ctx.send("Please attach a .csv file to load questions from.")
+        if len(ctx.message.attachments) > 1:
+            return await ctx.send("Please attach a single file.")
+        data = await ctx.message.attachments[0].read().split('\n')[1:]
+        records = [record.split(',') for record in data]
+        async with self._db.acquire() as conn:
+            id = await conn.fetchval("INSERT INTO quiz_config (name, description) VALUES ($1, $2) RETURNING id", ctx.message.attachments[0].filename[:-4], desc)
+            for record in records:
+                try:
+                    await self._db.execute("INSERT INTO questions (quiz_id, question, correct, option1, option2, option3, option4) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+                            id, record[0], int(record[5]), record[1], record[2], record[3], record[4])
+                except:
+                    await self._db.execute("DELETE FROM quiz_config WHERE id = $1", id)
+                    await self._db.execute("DELETE FROM questions WHERE quiz_id = $1", id)
+                    return await ctx.send("Something went wrong while processing your file. Please try again.")
+        self.ids.append(id)        
+        await ctx.send(f"Loaded quiz into database with id **{id}**. Please make note of this as it is needed while starting the quiz.")
+
+    @commands.command(name="start")
+    @commands.guild_only()
+    async def _start(self, ctx, id, score_type='static'):
+
+        if self.in_play:
+            return await ctx.send("A quiz is already in progress")
+        try:
+            if int(id) not in self.ids:
+                return await ctx.send("Quiz with this ID does not exist.")
+        except ValueError:
+            return await ctx.send("Please enter a valid number for ID.")
+        if score_type.lower() not in ['static', 'dynamic']:
+            return await ctx.send("Specify a valid scoring type (static/dynamic)")
+
+        quiz_data = await self._db.fetchrow("SELECT * FROM quiz_config WHERE id = $1", id)
+        name = quiz_data['name']
+        desc = quiz_data['description']
+        self.questions = []
+        ques_records = await self._db.fetch("SELECT * FROM questions WHERE quiz_id = $1", id)
+        for record in ques_records:
+            questions.append({
+                'question' : record['question'],
+                '1' : record['option1'],
+                '2' : record['option2'],
+                '3' : record['option3'],
+                '4' : record['option4']
+                'Correct' : record['correct']})
+        random.shuffle(self.questions)    
+        await ctx.send("Questions loaded. Starting in 5 seconds")
+        self.in_play = True
+        self._lb = dict()
+        await asyncio.sleep(5)
+        for num, question in enumerate(questions, 1):
+            reactions = []
+            self._score = MAX_POINTS
+            embed = discord.Embed(title=f"Question {num}:", description=question['question'], colour=discord.Colour.green())
+            if question['1'] != '':
+                embed.add_field(name="1\N{variation selector-16}\N{combining enclosing keycap}", value=question['1'])
+                reactions.append("1\N{variation selector-16}\N{combining enclosing keycap}")
+            if question['2'] != '':    
+                embed.add_field(name="2\N{variation selector-16}\N{combining enclosing keycap}", value=question['2'])
+                reactions.append("2\N{variation selector-16}\N{combining enclosing keycap}")
+            if question['3'] != ''    
+                embed.add_field(name="3\N{variation selector-16}\N{combining enclosing keycap}", value=question['3'])
+                reactions.append("3\N{variation selector-16}\N{combining enclosing keycap}")
+            if question['4'] != '':
+                embed.add_field(name="4\N{variation selector-16}\N{combining enclosing keycap}", value=question['4'])
+                reactions.append("4\N{variation selector-16}\N{combining enclosing keycap}")
+            msg = await ctx.send(embed=embed)
+            for reaction in reactions:
+                await msg.add_reaction(reaction)
+            self._current = [msg.id, question['Correct'], []]
+            for _ in range(TIMEOUT):
+                await asyncio.sleep(1)
+                if score_type.lower() == 'dynamic':
+                    self._score -= MAX_POINTS / TIMEOUT
+            self._current = []
+            await ctx.send(embed=discord.Embed(title=f"Question {num}:", description=f"{question['question']}\n\n:white_check_mark: {question['correct']}", colour=discord.Colour.red()))
+                    
+        await self._publish(ctx)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if not self._current:
+            return
+        if not payload.message_id == self._current[0] or not self.in_play:
+            return
+        if payload.user_id in self._current[2]:
+            return
+        message_dict = {
+                "1\N{variation selector-16}\N{combining enclosing keycap}" : 1,
+                "2\N{variation selector-16}\N{combining enclosing keycap}" : 2,
+                "3\N{variation selector-16}\N{combining enclosing keycap}" : 3,
+                "4\N{variation selector-16}\N{combining enclosing keycap}" : 4}
+        if message_dict.get(str(payload.emoji)) == self._current[1] and payload.user_id != self.bot.user.id:
+            self._lb[payload.user_id] = self._lb.get(payload.user_id, 0) + self._score
+        if not payload.user_id == self.bot.user.id:
+            await self.bot.get_channel(payload.channel_id).get_message(payload.message_id).remove_reaction(str(reaction.emoji), self.bot.get_user(payload.user_id))
+            self._current[3].append(payload.user_id)
+    
+    async def _publish(self, ctx):
+        self._current = []
+        self.in_play = False
+        await ctx.send("Quiz concluded. Preparing results... (If this message is unexpected or premature, the bot has suffered downtime. Apologies for the inconvenience")
+        desc = '\n'.join([f"{self.bot.get_user(user_id).mention}: {score}" for user_id, score in sorted([(user_id, score) for user_id, score in self._lb.items()], key=lambda x: x[1])])
+        embed = discord.Embed(title="Final results:", description = desc, colour=discord.Colour.blurple())
+        # TODO embellish embeds
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Quiz(bot))
